ef9bf0e699901082d71dad970852d57d24e592ba
s-kondyli <100346881+s-kondyli@users.noreply.github.com>
2022-05-10 22:01:18 +0200

    Create side_by_side_barplots.py

---
+++ side_by_side_barplots.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+data = pd.read_excel('C:/Users/lilak/Documents/Master BMS-O/Research Project 1/Results from assembly step/contigs_reads.xlsx', sheet_name='Kraken_EukRep')
+labels = data['sample'].tolist()
+x = np.arange(22)
+w = 0.5
+plt.bar(x,data.Percent_overlap, color='g', label='kraken',width=w)
+plt.bar(x+w,data.Percent_overlap_EukRep, color='b', label='EukRep', width=w)
+plt.xticks(ticks=x, labels=labels, rotation=30, fontsize=5)
+plt.legend()
+plt.tight_layout()
+plt.show()
