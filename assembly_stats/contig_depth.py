32656fd6ac47a9284ce8febbaed3e01f82737d99
s-kondyli <100346881+s-kondyli@users.noreply.github.com>
2022-05-15 22:34:16 +0200

    Update contig_depth.py

--- assembly_stats/contig_depth.py
+++ assembly_stats/contig_depth.py
@@ -5,6 +5,5 @@ for i in sys.argv[1:-1]:
     data_stats = data.describe()
     x = data.iloc[1,0].split('_')
     sheetname = x[0]
-    print(data_stats)
     with pd.ExcelWriter(sys.argv[-1], mode='a',engine='openpyxl') as writer:  # append an existing excel file which matches the output we gave in the command line
         data_stats.to_excel(writer, sheet_name=sheetname, index=True) # index true cause it takes the count max mean etc as index and we want it 
