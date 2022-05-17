50300f543deb85aad63868e8eb1dd031e22a47ab
s-kondyli <100346881+s-kondyli@users.noreply.github.com>
2022-05-17 18:36:27 +0200

    Update heatmap_part2.py

--- heatmap_part2.py
+++ heatmap_part2.py
@@ -1,22 +1,29 @@
 import pandas as pd
+import sys
 import matplotlib.pyplot as plt
 import seaborn as sns
 from skbio.stats.composition import clr, multiplicative_replacement
-data = pd.read_csv('C:/Users/lilak/Documents/Master BMS-O/Research Project 1/mOTUs/1179_ordered.csv')
-data = data.set_index('Name') #reseting the index
-index = data.index # storing the index into a list for the scaled_df for the plot 
-columns = data.columns.tolist() # storing the columns into a list for the scaled_df for the plot 
-# Log-scaling the data 
-data = multiplicative_replacement(data) # replaces all zeros with small-non-zero-values
-data = clr(data) # center-log ratio transformation
-# Creating the new scaled_df 
-scaled_df = pd.DataFrame(data, index=index, columns=columns)
-# Plotting the heatmap 
-sns.heatmap(scaled_df, cmap='BuPu', linewidth=0.7, yticklabels=True, xticklabels=True) #yticklabels for all taxa to appear
-plt.xticks(rotation=0, fontsize=4) # rotation & size of the ticks
-plt.yticks(rotation=4, fontsize=6)
-plt.ylabel('Taxa')
-plt.xlabel('Day related to HCT')
-plt.title('Patient 1179 Heatmap')
-plt.tight_layout()
-plt.savefig('C:/Users/lilak/Documents/Master BMS-O/Research Project 1/Python/Plots/Patient_1179_Heatmap.png' , dpi=1200)
+#importing our data
+sheet_dict = pd.read_excel(sys.argv[1], sheet_name=None)
+for key, value in sheet_dict.items():
+    patient = key
+    value['name'] = value['name'].str.split('\\[ref').str[0] # \\ because [ is a special character
+    value['name'] = value['name'].str.split('\\[met').str[0]
+    value = value.set_index('name')   #reseting the index
+    index = value.index # storing the index into a list for the scaled_df for the plot
+    columns = value.columns.tolist() # storing the columns into a list for the scaled_df for the plot
+    data = multiplicative_replacement(value)  # replaces all zeros with small-non-zero-values
+    data = clr(data) # center-log ratio transformation
+    scaled_df = pd.DataFrame(data, index=index, columns=columns) # creating the scaled_df
+    sns.heatmap(scaled_df, cmap='YlGn', linewidth=0.7, yticklabels=True, square=True)  # yticklabels for all taxa to appear
+    plt.xticks(rotation=0, fontsize=6)  # rotation & size of the ticks
+    plt.yticks(rotation=4, fontsize=8)
+    plt.ylabel('Taxa')
+    plt.xlabel('Day related to HCT')
+    plt.title('Patient' + ' ' + patient + ' ' + 'Heatmap') # mona autakia gia to keno
+    plt.tight_layout()
+    plt.show()
+
+
+exit()
+
