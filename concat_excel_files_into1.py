274bdde25e6887d8b43c94ab9d0a0478e4dfbc1f
s-kondyli <100346881+s-kondyli@users.noreply.github.com>
2022-05-22 17:20:32 +0200

    Update concat_excel_files_into1.py

--- concat_excel_files_into1.py
+++ concat_excel_files_into1.py
@@ -1,1 +1,9 @@
+import pandas as pd
+import sys
+# input files sys.argv[1:-1]
+# output file the last sys.argv
+# conctatenates excel files with single sheets into 1 excel workbook (1sheet)
+data = pd.read_csv(sys.argv[1], sep='\t', header=None, names=['kmer'])
+data[['kmer', 'freq']] = data['kmer'].str.split(' ',1,expand=True)  #split the column where you have space & save the second element of the list in another column called 'freq'
+data.to_excel('Saccharomyces_cerevisiae .xlsx', index=False)
 
