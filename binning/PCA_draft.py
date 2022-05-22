b22b532a13a49f362f365efddf6d142147fab836
s-kondyli <100346881+s-kondyli@users.noreply.github.com>
2022-05-22 22:51:38 +0200

    Update PCA_draft.py

--- binning/PCA_draft.py
+++ binning/PCA_draft.py
@@ -1,22 +1,20 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-from pca import pca
-from skbio.stats.composition import clr, multiplicative_replacement
 import sys
-data = pd.read_excel(sys.argv[1], header=None, names=['kmer','freq','taxon'])
-
+from pca import pca
+from sklearn.preprocessing import StandardScaler
+from skbio.stats.composition import clr, multiplicative_replacement
+# Import our data
+data = pd.read_excel(sys.argv[1],  header=None, names=['kmer','freq','taxon'])
 data = data.pivot(index='taxon', columns='kmer', values='freq')
-
-data.to_csv('temp.csv')
-data = pd.read_csv('temp.csv')
-
+features = data.columns.tolist()
+data = data.reset_index()
+data['taxon'] = data['taxon'].replace(regex=r'^Candida_dubliniensis[0-9]$', value='Candida_dubliniensis')
+data['taxon'] = data['taxon'].replace(regex=r'^Saccharomyces_cerevisiae[0-9]$', value='Saccharomyces_cerevisiae')
+data['taxon'] = data['taxon'].replace(regex=r'^Malassezia_restricta[0-9]$', value='Malassezia_restricta')
+samples = data['taxon']
 X = data.drop('taxon', axis=1)
-#Replace all zeros with small non-zero values
-X = multiplicative_replacement(X)
-# Performs centre log ratio transformation
-X = clr(X)
-labels = data['taxon']
-model = pca(n_components=2, normalize=True)
-results = model.fit_transform(X)
-model.biplot(n_feat=4, y=labels, legend=False)
-plt.show()
+scaler = StandardScaler(with_mean=True, with_std=True)
+X = scaler.fit_transform(X)
+model = pca(normalize=True)
+results = model.fit_transform(X, row_labels=samples, col_labels=features)
+model.biplot(n_feat=3, label=False, legend=True)
